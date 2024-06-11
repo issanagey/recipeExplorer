55b92317c6472393984eb262207e1e1b9cee5c05
Lim <limzhengzhian123@gmail.com>
2024-06-11 20:50:34 +0800

    login + database integration with cache

--- app/src/main/java/com/example/recipeexplorer/database/DatabaseManager.java
+++ app/src/main/java/com/example/recipeexplorer/database/DatabaseManager.java
@@ -16,13 +16,13 @@ public class DatabaseManager {
     }
 
     // Method to add a user to the database
-    public void addUser(String name, String email, String password, byte[] profilePicture) {
+    public void addUser(String name, String password, byte[] profilePicture) {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         ContentValues values = new ContentValues();
         values.put("name", name);
-        values.put("email", email);
         values.put("password", password);
+        values.put("challenge_completed", 0);
         values.put("profile_picture", profilePicture);
 
         long newRowId = db.insert("users", null, values);
@@ -34,11 +34,8 @@ public class DatabaseManager {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
         String[] projection = {
-                "id",
                 "name",
-                "email",
                 "password",
-                "profile_picture"
         };
 
         Cursor cursor = db.query(
@@ -54,12 +51,9 @@ public class DatabaseManager {
         while (cursor.moveToNext()) {
             long userId = cursor.getLong(cursor.getColumnIndexOrThrow("id"));
             String userName = cursor.getString(cursor.getColumnIndexOrThrow("name"));
-            String userEmail = cursor.getString(cursor.getColumnIndexOrThrow("email"));
-            String userPassword = cursor.getString(cursor.getColumnIndexOrThrow("password"));
-            byte[] userProfilePicture = cursor.getBlob(cursor.getColumnIndexOrThrow("profile_picture"));
 
             // Handle the retrieved data (e.g., log it or display it)
-            Log.d("Database", "ID: " + userId + " Name: " + userName + " Email: " + userEmail);
+            Log.d("DatabaseManager", "ID: " + userId + " Name: " + userName);
         }
         cursor.close();
     }
@@ -70,8 +64,9 @@ public class DatabaseManager {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
         String[] projection = {
+                "id",
                 "name",
-                "password",
+                "password"
         };
 
         Cursor cursor = db.query(
@@ -92,6 +87,11 @@ public class DatabaseManager {
 
             // check if username & pw match
             if(userName.equals(name) && userPassword.equals(password)){
+
+                // put id into login list (currently logged in)
+                int userId = cursor.getInt(cursor.getColumnIndexOrThrow("id"));
+                loginUser(userId);
+
                 cursor.close();
                 return true;
             }
@@ -136,6 +136,125 @@ public class DatabaseManager {
         // no matches found
         cursor.close();
         return true;
+    }
+
+    public void loginUser(long id) {
+        // open in write mode
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // delete all rows in table
+        db.delete("login", null, null);
+
+        // put in new user
+        ContentValues values = new ContentValues();
+        values.put("id", id);
+
+        db.insert("login", null, values);
+    }
+
+    // logout user (only from database and not views !!! always reset to Login page when calling this method)
+    public void LogoutUser() {
+        // open in write mode
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // delete all rows in table
+        db.delete("login", null, null);
+
+    }
+
+    // returns true if there is currently a user logged in
+    // allows for app to skip login page
+    public boolean IsLoggedIn(){
+        // check if login table is empty
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                "id"
+        };
+
+        Cursor cursor = db.query(
+                "login",
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        if(cursor.getCount() == 0){
+            cursor.close();
+            return false;
+        }
+        cursor.close();
+        return true;
+    }
+
+    public int GetCurrentUserID(){
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                "id"
+        };
+
+        Cursor cursor = db.query(
+                "login",
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        while (cursor.moveToNext()) {
+
+            // get id
+            String id_str = cursor.getString(cursor.getColumnIndexOrThrow("id"));
+            int id = Integer.parseInt(id_str);
+
+            cursor.close();
+            return id;
+        }
+
+        // no user currently logged in
+        cursor.close();
+        return -1;
+    }
+
+    public String GetUsername(int ID){
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                "id",
+                "name"
+        };
+
+        Cursor cursor = db.query(
+                "users",
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        while (cursor.moveToNext()) {
+
+            long userId = cursor.getLong(cursor.getColumnIndexOrThrow("id"));
+
+            // check id
+            if(userId == ID){
+                String userName = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+                cursor.close();
+                return userName;
+            }
+        }
+
+        // no user found
+        cursor.close();
+        return "No Match";
     }
 
     // Add for recipes, challenges, and achievements tables as needed.
